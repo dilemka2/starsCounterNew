da7ac68f975d170ffa68b835e323825f07f948c3
sbohdano <24v_sbs@liceum.ztu.edu.ua>
2025-02-12 18:44:52 +0200

    next update

--- node.js
+++ node.js
@@ -268,13 +268,13 @@ app.post('/login', async (req, res) => {
                 })
             }
             let user = result[0];
-            if (password != user.password) {
+            if (req.body.password != user.password) {
                 return res.render('login', {
                     message: 'Wrong password'
                 })
             }
 
-            if (password == user.password) {
+            if (req.body.password == user.password) {
                 req.session.userId = user.id;
                 return res.render('index', { account: 'is', login: login })
             }
